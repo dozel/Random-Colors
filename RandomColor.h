180c6c55522cb7debb09f6b6d00d580757b958d0
dozel <dcozel@gmail.com>
2013-01-02 10:23:10 -0800

    Create RandomColor.h

---
+++ RandomColor.h
@@ -0,0 +1,15 @@
+//
+//  RandomColor.h
+//  
+//
+//  Created by Devin Ozel on 1/2/13.
+//  
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RandomColor : NSObject
+
++ (UIColor *) generateRandomColor;
+
+@end
